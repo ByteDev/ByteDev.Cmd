39daaf12aeb62f0ca32b6d3b74a0132ffaaa86a6
bytedev <paulstanley@protonmail.com>
2019-10-06 13:43:35 +0800

    Added missing exception

---
+++ src/ByteDev.Cmd/Arguments/CmdArgException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ByteDev.Cmd.Arguments
+{
+    [Serializable]
+    public class CmdArgException : Exception
+    {
+        public CmdArgException()
+        {
+        }
+
+        public CmdArgException(string message) : base(message)
+        {
+        }
+
+        public CmdArgException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected CmdArgException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
